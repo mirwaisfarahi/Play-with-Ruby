34d829dda4aa3b0b7bd9bb0687204d84ef41bd1f
Mirwais Farahi <mirfarahi92@gmail.com>
2023-02-04 19:11:17 +0500

    Add main.rb File

---
+++ main.rb
@@ -0,0 +1,20 @@
+> print "Hello World"
+Hello World => nil
+
+2+2
+10/3
+10/3.0
+"Happy" + "coding"
+"Happy" + "\n" +  "coding"
+print "Happy" + "\n" +  "coding" + "\n"
+
+a = 3
+a
+a = "Hello"
+a
+b = "World!"
+puts(a + b)
+
+irb(main)> def hello(name)
+irb(main)>   puts("Hello #{name}!")
+irb(main)> end
